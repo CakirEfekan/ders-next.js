9bb4b4b80d358a998f2555787f7f6a950a739f86
cakirefekan <cakirefekan@gmail.com>
2020-07-31 18:34:14 +0300

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ function Layout({ children }) {
   return (
     <div>
       <Head>
-        <title>Adem İlter'in web sitesi</title>
+        <title>Efekan Çakır'ın web sitesi</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Navigation />
